c0fae21d7ccf6c687751e0ac887ebd1185be9f9b
Valentin Roman <romann.valentin@gmail.com>
2021-03-06 13:13:42 -0500

    Adaugat codul exemplu

---
+++ Lab2/CodExemplu/src/Polyglot.java
@@ -0,0 +1,53 @@
+//import libraria principala polyglot din graalvm
+import org.graalvm.polyglot.*;
+
+//clasa principala - aplicatie JAVA
+class Polyglot {
+    //metoda privata pentru conversie low-case -> up-case folosind functia toupper() din R
+    private static String RToUpper(String token){
+        //construim un context care ne permite sa folosim elemente din R
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        //folosim o variabila generica care va captura rezultatul excutiei funcitiei R, toupper(String)
+        //pentru aexecuta instructiunea I din limbajul X, folosim functia graalvm polyglot.eval("X", "I");
+        Value result = polyglot.eval("R", "toupper(\""+token+"\");");
+        //utilizam metoda asString() din variabila incarcata cu output-ul executiei pentru a mapa valoarea generica la un String
+        String resultString = result.asString();
+        // inchidem contextul Polyglot
+        polyglot.close();
+
+        return resultString;
+    }
+
+    //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
+    private static int SumCRC(String token){
+        //construim un context care ne permite sa folosim elemente din PYTHON
+        Context polyglot = Context.newBuilder().allowAllAccess(true).build();
+        //folosim o variabila generica care va captura rezultatul excutiei functiei PYTHON, sum()
+        //avem voie sa inlocuim anumite elemente din scriptul pe care il construim spre evaluare, aici token provine din JAVA, dar va fi interpretat de PYTHON
+        Value result = polyglot.eval("python", "sum(ord(ch) for ch in '" + token + "')");
+        //utilizam metoda asInt() din variabila incarcata cu output-ul executiei, pentru a mapa valoarea generica la un Int
+        int resultInt = result.asInt();
+        // inchidem contextul Polyglot
+        polyglot.close();
+
+        return resultInt;
+    }
+
+    //functia MAIN
+    public static void main(String[] args) {
+        //construim un context pentru evaluare elemente JS
+        Context polyglot = Context.create();
+        //construim un array de string-uri, folosind cuvinte din pagina web:  https://chrisseaton.com/truffleruby/tenthings/
+        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",\"command\",\"included\",\"in\",\"GraalVM\",\"we\",\"will\",\"be\",\"automatically\",\"using\",\"the\",\"Graal\",\"JIT\",\"compiler\",\"no\",\"extra\",\"configuration\",\"is\",\"needed\",\"I\",\"will\",\"use\",\"the\",\"time\",\"command\",\"to\",\"get\",\"the\",\"real\",\"wall\",\"clock\",\"elapsed\",\"time\",\"it\",\"takes\",\"to\",\"run\",\"the\",\"entire\",\"program\",\"from\",\"start\",\"to\",\"finish\",\"rather\",\"than\",\"setting\",\"up\",\"a\",\"complicated\",\"micro\",\"benchmark\",\"and\",\"I\",\"will\",\"use\",\"a\",\"large\",\"input\",\"so\",\"that\",\"we\",\"arent\",\"quibbling\",\"about\",\"a\",\"few\",\"seconds\",\"here\",\"or\",\"there\",\"The\",\"large.txt\",\"file\",\"is\",\"150\",\"MB\"];");
+        //pentru fiecare cuvant, convertim la upcase folosind R si calculam suma de control folosind PYTHON
+        for (int i = 0; i < array.getArraySize();i++){
+            String element = array.getArrayElement(i).asString();
+            String upper = RToUpper(element);
+            int crc = SumCRC(upper);
+            System.out.println(upper + " -> " + crc);
+        }
+        // inchidem contextul Polyglot
+        polyglot.close();
+    }
+}
+
